e5565f4b5adf004d71b62611c081311ec2fa0e30
apostolidoum <apostolidou9mar@gmail.com>
2026-06-14 17:24:49 +0300

    fix code to handle mwt

--- swap.py
+++ swap.py
@@ -11,7 +11,14 @@ def swap_columns_and_update_text(input_file_path, output_file_path):
     for sentence in sentences:
         new_words = []
 
+        # Track which IDs are part of an MWT split so we can skip them
+        ids_to_skip = set()
         for token in sentence:
+            if isinstance(token["id"], tuple):
+                start_id = int(token["id"][0])
+                end_id = int(token["id"][2])
+                ids_to_skip.update(range(start_id, end_id + 1))
+
             # CoNLL-U columns are 0-indexed in python-conllu:
             # Column 2 (FORM) is token['form']
             # Column 10 (MISC) is token['misc']
@@ -34,7 +41,15 @@ def swap_columns_and_update_text(input_file_path, output_file_path):
             token["form"] = misc_as_string
             token["misc"] = old_form
 
-            new_words.append(text_word)
+            try:
+                current_id = int(token["id"])
+            except TypeError, ValueError:
+                current_id = token["id"]
+
+            if current_id in ids_to_skip:
+                continue
+            else:
+                new_words.append(text_word)
 
         sentence.metadata["text"] = " ".join(new_words).strip()
 
